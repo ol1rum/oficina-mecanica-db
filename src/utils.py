cdc4d79ca5541bd595bd714c95718c68cfc353cf
Murilo <mimetw73@gmail.com>
2026-04-09 01:06:27 -0300

    feat(utils): remove variáveis de caminho SQL

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,7 @@ from pathlib import Path
 
 CAMINHO_ROOT = Path(__file__).resolve().parent.parent
 CAMINHO_SQL = CAMINHO_ROOT / 'sql'
-CAMINHO_SQL_QUERIES = CAMINHO_SQL / 'queries'
-CAMINHO_SQL_COMMANDS = CAMINHO_SQL / 'commands'
 
 if __name__ == '__main__':
     print(CAMINHO_ROOT)
     print(CAMINHO_SQL)
-    print(CAMINHO_SQL_QUERIES)
-    print(CAMINHO_SQL_COMMANDS)
