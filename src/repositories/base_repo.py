4b4da603adef0662edd2ce2a31b364a750ecf16c
Murilo <mimetw73@gmail.com>
2026-04-12 02:46:17 -0300

    fix(base-repo): corrige parâmetros da função execSql para melhor legibilidade e conserta problema de tupla dentro de tupla

--- src/repositories/base_repo.py
+++ src/repositories/base_repo.py
@@ -7,8 +7,8 @@ class BaseRepository:
     def __init__(self, pasta_sql: str) -> None:
         self.db: DatabaseManager = DatabaseManager(CAMINHO_SQL / pasta_sql)
 
-    def execSql(self, arquivo_sql: str, *param: tuple, commit: bool = False) -> int:
+    def execSql(self, arquivo_sql: str, parametros: tuple = (), commit: bool = True) -> int:
         """Executa um arquivo SQL não SELECT com os parâmetros fornecidos e retorna o id de alteração."""
-        res: dict = self.db.executar(arquivo_sql=arquivo_sql, parametros=(*param,), autocommit=commit)
+        res: dict = self.db.executar(arquivo_sql=arquivo_sql, parametros=parametros, autocommit=commit)
         return res[0]
     
