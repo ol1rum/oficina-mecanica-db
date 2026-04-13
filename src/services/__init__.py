5c93ab139e4926fd7e0324ecac4090a3581e13d5
Murilo <mimetw73@gmail.com>
2026-04-13 03:31:41 -0300

    feat(service): adicionar __init__.py ao pacote de servicos

---
+++ src/services/__init__.py
@@ -0,0 +1,5 @@
+from .cliente_service import ClienteService
+from .veiculo_service import VeiculoService
+from .os_service import OrdemServicoService
+from .mecanico_service import MecanicoService
+from .servicos_service import ServicoService
